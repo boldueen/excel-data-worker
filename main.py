d3986b7a6959493ecb0eb4bffc758f61df57e060
boldueen <83167994+boldueen@users.noreply.github.com>
2022-04-04 12:54:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from utils import make_report
 
 def start():
 
-    file = 'C:\work\Orders1.xlsx'
+    file = ''
     logging.basicConfig(level=logging.INFO, format='%(asctime)s.%(msecs)03d %(message)s', datefmt='%I:%M:%S')
     logging.info("program has been started successfully")
     logging.info(make_report(file))
